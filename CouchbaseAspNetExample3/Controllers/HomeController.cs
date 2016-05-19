8d3db5811caff4cbb247793fca5d1349a12d091f
Matthew Groves <me@mgroves.com>
2016-05-19 10:28:35 -0400

    initial commit of example code and placeholder readme

---
+++ CouchbaseAspNetExample3/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Web.Mvc;
+using CouchbaseAspNetExample3.Models;
+
+namespace CouchbaseAspNetExample3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PersonRepository _personRepo;
+
+        public HomeController(PersonRepository personRepo)
+        {
+            _personRepo = personRepo;
+        }
+
+        public ActionResult Index()
+        {
+            var list = _personRepo.GetAll();
+            return View(list);
+        }
+
+        public ActionResult Add()
+        {
+            return View("Edit", new Person());
+        }
+
+        [HttpPost]
+        public ActionResult Save(Person model)
+        {
+            _personRepo.Save(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            var person = _personRepo.GetPersonByKey(id);
+            return View("Edit", person);
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            _personRepo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CouchbaseAspNetExample3/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Couchbase;
+using Couchbase.Configuration.Client;
+using StructureMap.Web.Pipeline;
+
+namespace CouchbaseAspNetExample3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var config = new ClientConfiguration();
+            config.Servers = new List<Uri>
+            {
+                new Uri("http://localhost:8091")
+            };
+            config.UseSsl = false;
+            ClusterHelper.Initialize(config);
+        }
+
+        protected void Application_End()
+        {
+            ClusterHelper.Close();
+        }
+
+        protected void Application_EndRequest()
+        {
+            HttpContextLifecycle.DisposeAndClearAll();
+        }
+    }
+}
